cfefe3f2aa0ac7dedff309b6108f02f26ac02589
Vishal-thakur22 <vishalthakur220103@gmail.com>
2025-01-27 19:49:36 +0530

    new

--- javascript/1java.js
+++ javascript/1java.js
@@ -1,7 +1,35 @@
-console.log("hello this is my 1 st commit");
-const name = "John";
-console.log(name);
+// console.log("hello this is my 1 st commit");
+// const name = "John";
+// console.log(name);
   
-let mame
-console.log(mame);
-console.log("hello iam vishal")
+// let mame
+// console.log("mame");
+// console.log("hello iam vishal")
+
+
+
+// {
+//     let b =25;
+//     b=21;
+//     console.log(b);
+// }
+// for (let i = 0; i < 10; i++) { //it will print from 0-9 
+//     console.log(i)   
+// }
+// // for (const a = 0; a < 10; a++) {
+// //     console.log(a); //it will only print 0 and then will show error as const can be declared once 
+// // }
+
+
+
+// for (var b = 0; b < 10; b++) {
+//     console.log(b);
+// }
+// console.log(b); //as variable is indipendent of the "block space"  so it will make loop in the block and then the last  b will be equal to 10 then at last the value of b will be 10 and it will be removed from the condition of loop hence  will print 10    .
+
+
+
+
+// console.log(a);
+let a =21;
+console.log(a);
