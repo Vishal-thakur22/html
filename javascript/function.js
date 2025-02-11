80a07bc8341ce6bd3f99d514a8817924ca6801d3
Vishal-thakur22 <vishalthakur220103@gmail.com>
2025-02-11 11:23:17 +0530

    2feb

---
+++ javascript/function.js
@@ -0,0 +1,66 @@
+// function sum(a,b,c)//parameters
+// {
+//     // console.log(a+b+c);
+//     return a+b+c; //return the sum of a, b, and c.  after this no code will work
+// }
+// // sum(1,3);//function call
+// // sum();// gives NaN  ----->rule.
+// // sum(-1,-1);
+// sum(3,4,5);
+// console.log(r);
+
+
+// function sum(a=0,b=0,c) {
+//     console.log(a-b+c);
+// }
+// sum(1,9,5);
+
+
+
+// function cant be called without parenthesis.
+
+
+
+//                            //write a function to find the prime number //
+// let i =2;
+// function prime(a)
+//  {
+//     if (a<=1) {console.log(`${a} is not a prime number.`);   
+// }
+
+//    else if (a%i==0) {console.log(`${a} is not a prime number`);
+        
+//     } else if (a=2){
+//         console.log(`${a} is a prime number`);
+//     }
+
+    
+//     else {
+//         console.log(`${a} is  a prime number`);
+        
+//     }
+    
+// } 
+// prime(2);
+
+
+//                            // function to give the output of a die
+//  function RollDie() {
+//      let a = Math.floor( Math.random()*6)+1;
+
+//     if (a<=6) {
+//         console.log(a);
+//     } 
+//  }
+//  RollDie();//
+
+
+//                                          //arrow function//
+// let add=(a,b) => {
+//     return a+b
+// };
+// console.log(add(5,6)); // IF I WANT TO  do something with the return value I can do it like this.
+
+//
+//  let add=(a,b) => a+b;
+//  console.log(add(5,6));// if i want to directly print the value I can do it like this.
--- javascript/object.js
+++ javascript/object.js
@@ -1,26 +1,117 @@
-let info ={
-    name: "vishal thakur",
-    age:23,
-    marks:32,
-    books: ["hc verma","rd sharma", "honeycomb"],
-    hobbies:{       //this is nested objects as therre 
-        reading:"books",
-        playing:"volleyball",
-    }
-};
+// let info ={
+//     name: "vishal thakur",
+//     age:23,
+//     marks:32,
+//     books: ["hc verma","rd sharma", "honeycomb"],
+//     hobbies:{       //this is nested objects as therre 
+//         reading:"books",
+//         playing:"volleyball",
+//     }
+// };
 
-// console.log(info["age"]);
-// console.log(info.age);
-// console.log(info.books[1]);
+//                       //to get the keys of the object 
+  //console.log(Object.keys(info));
 
-// // info.marks=23; // if marks was not present in the object then it will add the new key in the info.or if present then will change the value of the key.
-
-// delete (info.marks);
-// delete info.age;   to delete the key from the object.
-
-delete info.name;
-console.log(info);
+//                       //to get all the values of the object
+  //console.log(Object.values(info));
 
 
 
+// // console.log(info["age"]);
+// // console.log(info.age);
+// // console.log(info.books[1]);
 
+// // // info.marks=23; // if marks was not present in the object then it will add the new key in the info.or if present then will change the value of the key.
+
+// // delete (info.marks);
+// // delete info.age;   to delete the key from the object.
+
+// delete info.name;
+// console.log(info);
+
+
+
+//   const classInfo= [{
+//     name:"vishal thakur",
+//     age:15,
+//     class:10,
+
+// },
+// {
+//     name:"dheeraj thakur",
+//     age:16,
+//     class:10
+// },
+// {
+//     name:"samar thakur",
+//     age:14,
+//     class:9,
+// },
+// ];
+
+// console.log(classInfo[2]);  // to access the object from the array
+// console.log(classInfo[2].name);  // to access the key from the object
+
+//                        //   to delete last element of array
+// classInfo.pop();
+// console.log(classInfo);
+
+
+
+
+
+// let a =2;
+// let b={};
+//  console.log(a==b);  // as it is object and have disfferent address.
+ 
+
+// let student ={...info , age:65}; // changes the age of the student object.
+// student.name= "aabc";
+// console.log(info);
+// console.log(student);
+
+ 
+// let { name,age,marks ,gmail} = info;  // destructuring the object.
+// console.log(name,age,marks);//print the values of the object.
+
+
+//
+//
+// //
+// let {name:username="hi", age, score:marks ,...data} = info;  // destructuring the object.
+
+// console.log(name);//name will not print as it will not find name but will find username as we have renamed it.
+
+// console.log(username); // print the values of the object.
+// console.log(age);
+// console.log(marks);// it will show undefined
+// console.log(data);
+
+// type of array is always object
+// console.log(typeof `${a}`); //string
+
+
+
+
+
+
+// outer function can call the inner function but the inner function can not call the outer function.//
+
+// function outer(){
+//   console.log("outer");
+//   return function inner(){
+//     console.log("inner");
+//   };
+// }
+//  let a =outer(); 
+//  a();             // this will call the inner functon 
+
+
+// function outer(a) {
+// console.log("outer"); // outer function
+// a();  
+// }
+// let inner = function(){
+//   console.log("inner");
+// };
+// outer(inner); // this will call the inner function.
